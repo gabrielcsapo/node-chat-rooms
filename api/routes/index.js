5f9f4774f6373daa3145da812a4e41af3eadf38e
Gabriel Csapo <gacsapo@paypalcorp.com>
2016-03-28 08:35:26 -1000

    adds route to badges /{room}.svg

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,24 @@
+var openBadge = require('openbadge');
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        // error
         res.send('error');
     });
-
+    app.get('/:room.svg', function(req, res){
+        var room = req.params.room;
+        openBadge({
+            text: ['chatter', room],
+            color:{
+                left:"#626262",
+                right:"#0188b3",
+                font:"#fff",
+                shadow:"#fff"
+            }
+        }, function (err, badgeSvg) {
+            res.set('Content-Type', 'image/svg+xml');
+            res.send(badgeSvg);
+        });
+    });
     app.get('/:room', function(req, res){
         var room = req.params.room;
         res.render('room', {title: room, message: 'welcome!', room: room});
